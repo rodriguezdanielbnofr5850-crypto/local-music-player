595e8ac65b34287950121d80ab2b0c03cb1ac2f6
white66-7 <rodriguezdanielbnofr5850@gmail.com>
2026-06-11 14:09:26 +0800

    完善目录栏组件，新增目录切换功能和样式；优化数据绑定逻辑

--- music-player/src/components/DirBar.vue
+++ music-player/src/components/DirBar.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+import { useLibraryStore } from '@/stores/libraryStore';
+  const libraryStore = useLibraryStore()
+
+  function switchDir(dirId: string) {
+  libraryStore.curDir = dirId
+  libraryStore.kw = ''
+}
+</script>
+
 <template>
-  <div></div>
+  <div class="dir-bar">
+    <div 
+    class="dir-chip"
+    :class="{on: useLibraryStore.curDir === 'all'}"
+    @click="switchDir('all')">
+  <img src="@/assets/file.png" alt="" class="file-logo"/>全部
+<span style="opacity: 0.7;">{{ libraryStore.library.length }}</span></div>
+<div v-for="d in libraryStore.dirs"
+:key="d.id"
+class="dir-chip"
+:class="{on: libraryStore.curDir === d.id}"
+@click="switchDir(d.id)"></div></div>
 </template>
